fixed rotation shapes problem

etl.py
@@ -48,9 +48,10 @@ def random_rotate(image, label):
     """Randomly rotate the image in 35 deg range"""
     def rotate_image(image):
         angle = np.random.uniform() * 35  # random angle from [0, 35]
-        return rotate(image.numpy(), angle)
+        return rotate(image.numpy(), angle, reshape=False)
 
     rotated = tf.py_function(rotate_image, [image], Tout=tf.float32)
+    rotated.set_shape([32, 32, 3])
 
     return rotated, label
 
@@ -68,8 +69,8 @@ def get_train_dataset():
     """Return train dataset after manipulations"""
     train_dataset = load_train_dataset()
     prep_train = train_dataset.map(normalize_image, num_parallel_calls=tf.data.experimental.AUTOTUNE)  # normalize
-    prep_train = prep_train.map(random_flip, num_parallel_calls=tf.data.experimental.AUTOTUNE)  # flip
     prep_train = prep_train.map(random_rotate, num_parallel_calls=tf.data.experimental.AUTOTUNE)  # rotate
+    prep_train = prep_train.map(random_flip, num_parallel_calls=tf.data.experimental.AUTOTUNE)  # flip
 
     return prep_train
 
@@ -87,6 +88,7 @@ if __name__=='__main__':
     fig, ax = plt.subplots(1, 3)
     for i, (image, label) in enumerate(ds.take(3)):
         ax[i].imshow(image)
+        #print(image.numpy().shape)
     plt.show()
 
 

models.py
@@ -216,11 +216,11 @@ class Inception(tf.keras.Model):
         root_x = self._root_conv(x)
 
         inception1 = self._inception1(root_x)
-        inception2 = self._inception2(inception1) + inception1
+        inception2 = self._inception2(inception1)
         mp1 = self._mp1(inception2)
 
         inception3 = self._inception3(mp1)
-        inception4 = self._inception4(inception3) + inception3
+        inception4 = self._inception4(inception3)
         mp2 = self._mp2(inception4)
 
         output = self._output(self._dropout(self._avg_pool(mp2)))